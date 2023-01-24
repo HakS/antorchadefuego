572d10964689cbd7e1959c599e4562710991619c
Juan Barba <juan.barba@redspace.com>
2023-01-24 16:58:59 -0500

    Arreglado error de imagen en el top, comenzando a construir el menu mobile

--- pages/Layout.tsx
+++ pages/Layout.tsx
@@ -10,46 +10,84 @@ type LayoutProps = {
   topContent ?: React.ReactNode;
   children: React.ReactNode;
 };
+
+const MenuLinks = () => {
+  return (
+    <>
+      <Link href="/emisora">Emisora</Link>
+      <Link href="/devocional">Devocional</Link>
+      <Link href="/donaciones">Donaciones</Link>
+      <Link href="/Contacto">Contacto</Link>
+    </>
+  );
+}
+
 const Layout = ({fullScreen = false, topContent, children}: LayoutProps) => {
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta property="og:site_name" content="Iglesia Antorcha de Fuego - Concilio Aposento Alto"></meta>
+        <meta
+          property="og:site_name"
+          content="Iglesia Antorcha de Fuego - Concilio Aposento Alto"
+        ></meta>
       </Head>
-      <div className={styles.mainWrapper}>
-        <header className={`${styles.mainHeader} ${fullScreen && styles.mainHeader__full}`}>
-          <div className={styles.mainHeader_inner}>
-            <Link href="/" className="mb-0 inline-flex">
-              <Image src="/logo.png" width={218} height={75} alt="Iglesia Antorcha de Fuego - Concilio Aposento Alto" />
-            </Link>
-            <nav>
-              <Link href="/emisora">Emisora</Link>
-              <Link href="/devocional">Devocional</Link>
-              <Link href="/donaciones">Donaciones</Link>
-              <Link href="/Contacto">Contacto</Link>
-            </nav>
+      <div className={styles.outerContainer}>
+        <nav className={styles.mobileSidebar}>
+          <MenuLinks />
+        </nav>
+        <div className={styles.pageWrap}>
+          <div className={styles.mainWrapper}>
+            <header
+              className={`${styles.mainHeader} ${
+                fullScreen && styles.mainHeader__full
+              }`}
+            >
+              <div className={styles.mainHeader_inner}>
+                <Link href="/" className="mb-0 inline-flex">
+                  <Image
+                    src="/logo.png"
+                    width={218}
+                    height={75}
+                    alt="Iglesia Antorcha de Fuego - Concilio Aposento Alto"
+                  />
+                </Link>
+                <nav>
+                  <MenuLinks />
+                </nav>
+              </div>
+            </header>
+            <div
+              className={`${styles.mainImage} ${
+                fullScreen && styles.mainImage__full
+              }`}
+            >
+              <Image
+                src={topImage}
+                fill={true}
+                alt="Iglesia Antorcha de Fuego - Concilio Aposento Alto"
+                placeholder="blur"
+              />
+            </div>
+            <div className={styles.topContent}>{topContent}</div>
+            <div className={styles.mainContent}>
+              <div
+                className={`${styles.pageMaxWidth} ${styles.mainContent_inner}`}
+              >
+                <main>{children}</main>
+              </div>
+            </div>
+            <div className={styles.mainFooter}>
+              <div className={`${styles.pageMaxWidth}`}>
+                <footer>
+                  Iglesia Antorcha de Fuego - Concilio Aposento Alto,{" "}
+                  {new Date().getFullYear()} todos los derechos reservados.
+                </footer>
+              </div>
+            </div>
           </div>
-        </header>
-        <div className={`${styles.mainImage} ${fullScreen && styles.mainImage__full}`}>
-          <Image src={topImage} fill={true} alt="Iglesia Antorcha de Fuego - Concilio Aposento Alto" placeholder="blur"/>
         </div>
-        <div className={styles.topContent}>
-          {topContent}
-        </div>
-        <div className={styles.mainContent}>
-          <div className={`${styles.pageMaxWidth} ${styles.mainContent_inner}`}>
-            <main>{children}</main>
-          </div>
-        </div>
-        <div className={styles.mainFooter}>
-          <div className={`${styles.pageMaxWidth}`}>
-            <footer>
-              Iglesia Antorcha de Fuego - Concilio Aposento Alto, {new Date().getFullYear()} todos los derechos
-              reservados.
-            </footer>
-          </div>
-        </div>
+        <div className={styles.pageOverlay}></div>
       </div>
     </>
   );
