4f4a125bde8b05411ac0bea1b400f6f0b540185c
Juan Barba <juan.barba@redspace.com>
2023-02-06 16:11:25 -0500

    Nuevo modo de incrustar font, actualizdo font a Futura usando CDN.

---
+++ pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <link href="https://fonts.cdnfonts.com/css/futura-std-4" rel="stylesheet" />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
