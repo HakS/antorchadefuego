2eee07dd31610550c4a590e7618240c269b6de6a
Juan Barba <juan.barba@redspace.com>
2023-02-08 20:29:37 -0500

    Terminada primera zona del homepage.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import { getAbsUrl } from '../utils';
 import Layout from './Layout';
 import styles from './index.module.scss';
 import AppHead from '../components/AppHead';
@@ -8,9 +7,7 @@ export default function Home() {
     <div className={styles.topContent}>
       <h1>Mateo 17:20</h1>
       <h2>
-        Porque de cierto os digo, que si tuviereis fe como un grano de
-        mostaza, diréis a este monte: Pásate de aquí allá, y se pasará; y nada
-        os será imposible.
+        Porque de cierto os digo, que si tuviereis fe como un grano de mostaza, diréis a este monte: Pásate de aquí allá, y se pasará; y nada os será imposible.
       </h2>
     </div>
   )
@@ -19,52 +16,24 @@ export default function Home() {
     <>
       <AppHead />
       <Layout fullScreen={true} topContent={topContent}>
-        <div className={`page-max-width page-padding ${styles.mainContent}`}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit
-          amet varius elit. Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit. Cras aliquet imperdiet risus ut venenatis. Etiam fermentum sed
-          nisi vitae tincidunt. Fusce volutpat ac justo quis pellentesque.
-          Mauris congue luctus velit non egestas. Morbi lacinia nisi mi, vitae
-          imperdiet massa pretium a. Duis ultrices justo at nisi tincidunt
-          aliquam. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Sed orci nulla, placerat eget ornare
-          nec, scelerisque quis magna. Suspendisse leo nibh, varius ut ipsum
-          sed, interdum tempus lorem. Suspendisse scelerisque orci et feugiat
-          condimentum. Quisque posuere augue leo, id semper lectus rutrum a. Nam
-          sollicitudin dignissim fringilla. Cras in erat hendrerit, imperdiet
-          ligula eu, posuere neque. Curabitur sit amet iaculis risus, at egestas
-          lacus. Cras consectetur nisi neque, condimentum suscipit augue
-          pellentesque nec. Duis purus sem, hendrerit id tempus ac, pellentesque
-          ac mi. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Curabitur gravida tempor enim at
-          condimentum. Maecenas varius lorem non tempus auctor. Nulla pharetra,
-          turpis id ornare pulvinar, ipsum velit fermentum lacus, a finibus
-          lectus libero id felis. Duis ut suscipit magna, ac auctor tellus.
-          Donec volutpat euismod tellus, ut eleifend eros dictum vel. Etiam
-          ultrices odio a tortor rhoncus, posuere pellentesque ante consectetur.
-          Suspendisse non massa suscipit felis posuere scelerisque. Nam ac
-          volutpat mauris, vel pellentesque justo. Vestibulum quis dignissim
-          sem. Mauris porttitor nisl ut nisi tempor dictum. Pellentesque vel
-          tristique mauris. Quisque ipsum lacus, congue vitae erat non,
-          vestibulum condimentum sem. Nulla facilisi. Ut a sodales justo.
-          Suspendisse tortor odio, dignissim quis felis nec, sollicitudin
-          sodales massa. Proin sit amet pharetra ex. Aenean eget risus risus.
-          Nullam tempor libero augue, id tincidunt nulla egestas id.
-          Pellentesque sit amet dictum dui. Nulla vel porttitor augue. Class
-          aptent taciti sociosqu ad litora torquent per conubia nostra, per
-          inceptos himenaeos. Quisque maximus ante eget eros aliquam, vel
-          fermentum est cursus. Nulla finibus nibh id sodales rhoncus. Donec
-          pellentesque quam dolor, in feugiat lorem semper vitae. Proin dapibus
-          massa consequat mi tempus, nec pretium eros semper. Integer sodales
-          dictum velit at cursus. Aenean commodo sed ligula at cursus. Aliquam
-          et ornare tellus. Nunc tempus magna id sapien tincidunt, in
-          condimentum tellus bibendum. Curabitur eget ornare odio. Quisque at
-          purus tristique quam tristique egestas eu ac sem. Pellentesque et
-          rhoncus augue. Vivamus maximus, lectus non blandit volutpat, libero
-          sapien sagittis tellus, eu vulputate nisl ipsum eu sem. Fusce ultrices
-          odio eros, eget semper justo viverra sed. Interdum et malesuada fames
-          ac ante ipsum primis in faucibus. Nullam tempor purus a mauris lacinia
-          ultrices. Suspendisse potenti.
+        <div className={styles.zone1}>
+          <section className={styles.aboutUs}>
+            <div className={styles.aboutUs_image}></div>
+            <h1 className={styles.aboutUs_title}>¡Gusto en conocerte!</h1>
+            <div className={styles.aboutUs_content}>
+              Te invitamos a que nos acompañes y que vivas una experienca única en el Señor. Una iglesia con corazón abierto para extender su mano de ayuda a las comunidades, haciendo participe a toda persona y nación de las buenas nuevas de salvación de nuestro Señor Jesucristo.
+            </div>
+          </section>
+          <section className={styles.services}>
+            <h1>Servicios</h1>
+            <ul>
+              <li>Ayunos: Martes y Sábados 9:30 am</li>
+              <li>Instituto Bíblico: Miércoles 7:30 pm</li>
+              <li>Servicio de Milagros: Viernes 7: 30 pm</li>
+              <li>Reunión de Jóvenes: Sábado 5:30pm</li>
+              <li>Escuela Dominical: 9:00 am</li>
+            </ul>
+          </section>
         </div>
       </Layout>
     </>
