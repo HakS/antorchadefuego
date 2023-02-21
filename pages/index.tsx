351d4aaa1a06d2abe44ffa5139a4f4c33de148e0
Juan Barba <juan.barba@redspace.com>
2023-02-20 21:11:52 -0500

    Cambios de estilo en front page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,9 +16,9 @@ export default function Home() {
     <>
       <AppHead />
       <Layout fullScreen={true} topContent={topContent}>
-        <div className={styles.zone1}>
+        <div className={`page-max-width page-padding ${styles.zone1}`}>
           <section className={styles.aboutUs}>
-            <div className={styles.aboutUs_image}></div>
+            {/* <div className={styles.aboutUs_image}></div> */}
             <h2 className={styles.aboutUs_title}>¡Gusto en conocerte!</h2>
             <div className={styles.aboutUs_content}>
               Te invitamos a que nos acompañes y que vivas una experienca única en el Señor. Una iglesia con corazón abierto para extender su mano de ayuda a las comunidades, haciendo participe a toda persona y nación de las buenas nuevas de salvación de nuestro Señor Jesucristo.
@@ -36,8 +36,9 @@ export default function Home() {
           </section>
         </div>
         <section className={styles.zone2}>
-          <h2>El mensaje de Dios a tu alcance</h2>
-          <h3>Visítanos o conéctate en linea para acompañarnos.</h3>
+          <div className={styles.zone2_image}></div>
+          <h2 className={styles.zone2_title}>El mensaje de Dios a tu alcance</h2>
+          <h3 className={styles.zone2_subtitle}>Visítanos o conéctate en linea para acompañarnos.</h3>
           <div className={styles.zone2_links}>
             <a className="button" href="https://www.google.com/maps/place/Church+Antorcha+de+Fuego/@11.0182756,-74.804673,19z/data=!4m5!3m4!1s0x8ef42d6c100599b7:0xc146c2b6690fc56a!8m2!3d11.0182756!4d-74.8046731" target="_blank" rel="noreferrer">Visítanos</a>
             <a className="button" href="https://www.youtube.com/@emisoraantorchadefuego2089/streams" target="_blank" rel="noreferrer">En vivo</a>
