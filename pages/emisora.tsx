71a3267d4227619404f890a8a73d54ff9bd690ef
Juan Barba <Juan.Barba.O@gmail.com>
2023-03-13 20:45:01 -0500

    Añadidos estilos para emisora, falta funcionalidad.

--- pages/emisora.tsx
+++ pages/emisora.tsx
@@ -1,57 +1,58 @@
 import AppHead from '../components/AppHead';
 import Layout from './Layout';
+import styles from "./emisora.module.scss";
+import { useState } from 'react';
 
 export default function Home() {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleRadio = () => {
+    if (isPlaying) {
+      setIsPlaying(false);
+    }
+    else {
+      setIsPlaying(true);
+    }
+  }
+
   return (
     <>
-      <AppHead title='Emisora' />
-      <Layout topContent='Emisora'>
-        <div className={`page-max-width page-padding`}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit
-          amet varius elit. Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit. Cras aliquet imperdiet risus ut venenatis. Etiam fermentum sed
-          nisi vitae tincidunt. Fusce volutpat ac justo quis pellentesque.
-          Mauris congue luctus velit non egestas. Morbi lacinia nisi mi, vitae
-          imperdiet massa pretium a. Duis ultrices justo at nisi tincidunt
-          aliquam. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Sed orci nulla, placerat eget ornare
-          nec, scelerisque quis magna. Suspendisse leo nibh, varius ut ipsum
-          sed, interdum tempus lorem. Suspendisse scelerisque orci et feugiat
-          condimentum. Quisque posuere augue leo, id semper lectus rutrum a. Nam
-          sollicitudin dignissim fringilla. Cras in erat hendrerit, imperdiet
-          ligula eu, posuere neque. Curabitur sit amet iaculis risus, at egestas
-          lacus. Cras consectetur nisi neque, condimentum suscipit augue
-          pellentesque nec. Duis purus sem, hendrerit id tempus ac, pellentesque
-          ac mi. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Curabitur gravida tempor enim at
-          condimentum. Maecenas varius lorem non tempus auctor. Nulla pharetra,
-          turpis id ornare pulvinar, ipsum velit fermentum lacus, a finibus
-          lectus libero id felis. Duis ut suscipit magna, ac auctor tellus.
-          Donec volutpat euismod tellus, ut eleifend eros dictum vel. Etiam
-          ultrices odio a tortor rhoncus, posuere pellentesque ante consectetur.
-          Suspendisse non massa suscipit felis posuere scelerisque. Nam ac
-          volutpat mauris, vel pellentesque justo. Vestibulum quis dignissim
-          sem. Mauris porttitor nisl ut nisi tempor dictum. Pellentesque vel
-          tristique mauris. Quisque ipsum lacus, congue vitae erat non,
-          vestibulum condimentum sem. Nulla facilisi. Ut a sodales justo.
-          Suspendisse tortor odio, dignissim quis felis nec, sollicitudin
-          sodales massa. Proin sit amet pharetra ex. Aenean eget risus risus.
-          Nullam tempor libero augue, id tincidunt nulla egestas id.
-          Pellentesque sit amet dictum dui. Nulla vel porttitor augue. Class
-          aptent taciti sociosqu ad litora torquent per conubia nostra, per
-          inceptos himenaeos. Quisque maximus ante eget eros aliquam, vel
-          fermentum est cursus. Nulla finibus nibh id sodales rhoncus. Donec
-          pellentesque quam dolor, in feugiat lorem semper vitae. Proin dapibus
-          massa consequat mi tempus, nec pretium eros semper. Integer sodales
-          dictum velit at cursus. Aenean commodo sed ligula at cursus. Aliquam
-          et ornare tellus. Nunc tempus magna id sapien tincidunt, in
-          condimentum tellus bibendum. Curabitur eget ornare odio. Quisque at
-          purus tristique quam tristique egestas eu ac sem. Pellentesque et
-          rhoncus augue. Vivamus maximus, lectus non blandit volutpat, libero
-          sapien sagittis tellus, eu vulputate nisl ipsum eu sem. Fusce ultrices
-          odio eros, eget semper justo viverra sed. Interdum et malesuada fames
-          ac ante ipsum primis in faucibus. Nullam tempor purus a mauris lacinia
-          ultrices. Suspendisse potenti.
+      <AppHead title="Emisora" />
+      <Layout topContent="Emisora Antorcha de Fuego">
+        <div className={`page-max-width ${styles.radio}`}>
+          <div className={styles.radio_audio} onClick={() => handleRadio()}>
+            <audio
+              src="https://radio30.virtualtronics.com/proxy/antorcha?mp=/stream"
+              preload="preload"
+            />
+            <img src="/emisora.svg" alt="" />
+            <span>
+              {isPlaying
+                ? "Click para detener la radio"
+                : "Click para iniciar la radio"}
+            </span>
+          </div>
+          <div className={styles.radio_livechat}>
+            <iframe
+              className={styles.cboxIframe}
+              src="https://www3.cbox.ws/box/?boxid=3526198&boxtag=ZEMvGR"
+              // src="https://www3.cbox.ws/box/?boxid=3442083&boxtag=2t592t&sec=form"
+              width="100%"
+              height="100%"
+              allow="autoplay"
+            ></iframe>
+            {/* <iframe
+              src="https://www3.cbox.ws/box/?boxid=3526198&boxtag=ZEMvGR&sec=form"
+              width="100%"
+              height="450"
+              allowtransparency="yes"
+              allow="autoplay"
+              frameborder="0"
+              marginheight="0"
+              marginwidth="0"
+              scrolling="auto"
+            ></iframe> */}
+          </div>
         </div>
       </Layout>
     </>
